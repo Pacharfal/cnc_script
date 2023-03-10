dbce60e62f87dd82d4ebb096b331d4528e3a0a36
Jan Kouřil <xkouril1@mendelu.cz>
2023-03-10 15:47:58 +0100

    function 1 almost done i will need some regex to make it work

--- script.py
+++ script.py
@@ -1,21 +1,38 @@
+#imports
+import re
+
+
 
 #funkce1 increments y by 10, where x is bigger than 50 and sort blocks in ascending order
+
 def funkce1():
     # open a file
     with open("D327971_fc1.i", "r") as input_file:
-        # Read the entire contents of the file into a string variable
-        file_contents = input_file.read()
+        file_content = input_file.read()
 
-    # Make modifications to the contents of the file as needed
-    modified_contents = file_contents.replace('X93.350Y116.850T01', 'Hello There !')
+    #split content to lines
+    lines = file_content.splitlines()
+
+    #Loop through each line
+    for i in range(len(lines)):
+        if "X" in lines[i]:
+            x = float(lines[i].split("X")[1].split("Y")[0])
+            if x > 50:
+                y = float(lines[i].split("Y")[1])
+                y += 10
+                lines[i] = lines[i].replace("Y" + str(int(y - 10)), "Y" + str(int(y)))
+
+    #join lines back together
+    new_content = "\n".join(lines)
 
     # Open a new file for writing
     with open('cnc.txt', 'w') as output_file:
         # Write the modified contents to the new file
-        output_file.write(modified_contents)
+        output_file.write(new_content)
     return None
 
 
 #funkce2 prints min/max values from x and y axes to console
+
 def funkce2():
     return None
